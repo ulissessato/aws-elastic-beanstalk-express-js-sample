fa72821e6ee075a59b3e50b899db1732d57c7f6e
ulissessato <107259419+ulissessato@users.noreply.github.com>
2022-08-29 15:19:08 -0300

    teste de reprova

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Testando PIPE'));
+app.get('/', (req, res) => res.send('Alterar errado'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
